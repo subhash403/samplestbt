f9ad96c500e596c8b302e927c4c31f3bee023fd3
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-03-21 20:55:11 -0600

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -7,13 +7,22 @@ def test_that_live_tv_is_playing():
     stbt.press('KEY_CLOSE')  # Close any open menus
     assert stbt.wait_for_motion()
 
-def test_VOD_VH_6119_tv_shows_launch():
+def test_VOD_ME_6119_tv_shows_launch():
     for _ in " "*3: stbt.press('KEY_EXIT')
     stbt.press('KEY_MENU')
     assert stbt.wait_for_match('images/menu_logo.png')
     stbt.press('KEY_DOWN')
     stbt.press('KEY_ENTER')
     assert stbt.wait_for_match('images/tv_shows_logo.png')
+    
+def test_VOD_ME_6120_check_tv_shows_filter():
+    for _ in " "*3: stbt.press('KEY_EXIT')
+    stbt.press('KEY_MENU')
+    assert stbt.wait_for_match('images/menu_logo.png')
+    stbt.press('KEY_DOWN')
+    stbt.press('KEY_ENTER')
+    assert stbt.wait_for_match('images/tv_shows_logo.png')
+    assert stbt.wait_for_match('images/included_with.png')
 
 def test_that_stb_tester_logo_is_shown():
     stbt.press('KEY_CHANNELUP')
