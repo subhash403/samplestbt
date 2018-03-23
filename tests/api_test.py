c0dcac89139b43ee20f1dbe9bdf331ae32590c20
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-03-23 12:02:10 -0600

    Update api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -13,15 +13,15 @@ def test_get_auth_token():
      return token
 
 def test_HDAT_setting():
-          token = test_get_auth_token()
+	token = test_get_auth_token()
 		#if value == "Off"
-			newAcctJson ='{"settings":{"groups":[{"id":"STB3438B77F88F8","type":"device-stb","options":[{"name":"HD Auto Tune","value":["Off"]}]}]}}'
+	newAcctJson ='{"settings":{"groups":[{"id":"STB3438B77F88F8","type":"device-stb","options":[{"name":"HD Auto Tune","value":["Off"]}]}]}}'
 		#elsif value == "On"
 			#newAcctJson ='{"settings":{"groups":[{"id":"STB3438B77F88F8","type":"device-stb","options":[{"name":"HD Auto Tune","value":["On"]}]}]}}'
 		#end
-          url = "http://spectrum.engprod-charter.net/api/pub/networksettingsedge/v1/settings"
-          headers={'X-CHARTER-SESSION':token}
-          req = requests.post(url, data=newAcctJson, auth=('charternet', 'Chart3rn3t'),headers=headers)
-          assert req.status_code == 200
+	url = "http://spectrum.engprod-charter.net/api/pub/networksettingsedge/v1/settings"
+	headers={'X-CHARTER-SESSION':token}
+	req = requests.post(url, data=newAcctJson, auth=('charternet', 'Chart3rn3t'),headers=headers)
+	assert req.status_code == 200
 
 
