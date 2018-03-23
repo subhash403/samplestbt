8f81a35311344b8d160ca8ebb309cd64db1d4551
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-03-23 14:36:00 -0600

    Update stress_test.py

--- tests/stress_test.py
+++ tests/stress_test.py
@@ -6,8 +6,19 @@ def test_weekend_run():
     stbt.press('KEY_GUIDE')
     assert stbt.wait_until(lambda: stbt.match("images/guide_options.png")), \
     "Guide not launched"
+    stbt.press('KEY_4')    
     stbt.press('KEY_SELECT')
-    stbt.press('KEY_RIGHT')
+    count =0
+    while true:
+        if assert stbt.wait_until(lambda: stbt.match("images/view_series_info.png")): break
+        else:
+            stbt.press('KEY_RIGHT')
+        count +=1
+        if count > 10:
+                assert stbt.wait_until(lambda: stbt.match("images/guide_options.png")), \
+                "Asset did not have a view series info cta"
+                
+    stbt.press('KEY_SELECT')            
     stbt.press('KEY_RIGHT')
     stbt.press('KEY_SELECT')
     stbt.press('KEY_OPTIONS')
