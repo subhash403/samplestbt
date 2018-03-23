66e8493a95fa9ee8de49864ea92ec668e416afcc
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-03-23 10:10:05 -0600

    Update api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -2,7 +2,7 @@ import requests
 
 def test_get_auth_token():
      req = requests.request("POST",url="http://spectrumint.engprod-charter.net/api/pub/loginedge/login/v1/auth/login", headers=
-                             "auth=HTTPBasicAuth('charternet', 'Chart3rn3t'))", params="macAddress=3438B77F88F8")
+                             {'charternet', 'Chart3rn3t'}, params="macAddress=3438B77F88F8")
      res=req.text
      print res
      return res
