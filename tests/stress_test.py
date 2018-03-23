9f56e6f6b1a921f52b7638e7f253b57c00195079
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-03-23 14:07:53 -0600

    Update stress_test.py

--- tests/stress_test.py
+++ tests/stress_test.py
@@ -6,4 +6,8 @@ def test_weekend_run():
     stbt.press('KEY_GUIDE')
     assert stbt.wait_until(lambda: stbt.match("images/guide_options.png")), \
     "Guide not launched"
+    stbt.press('KEY_SELECT')
+    stbt.press('KEY_RIGHT')
+    stbt.press('KEY_RIGHT')
+    stbt.press('KEY_SELECT')
     stbt.press('KEY_OPTIONS')
