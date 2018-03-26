a02bada1cf6cc80021abbb1ed5c821aaccb6a833
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-03-26 15:01:20 -0600

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -57,6 +57,10 @@ def send_command_internal(command, box_ip, timeout=10.0):
 def test_DVR_on_reboot():
     print("TESTEST")
     os.chdir("/var/lib/stbt/test-pack/tests")
+    p = subprocess.Popen('ls -lrt', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    for line in p.stdout.readlines():
+     print line,
+    retval = p.wait()
     subprocess.call(['./test1.sh'])
     count = 0
     while True:
