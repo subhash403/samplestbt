e53b4cbc58f7e812d4e020cb01bc50e0d3a12f19
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-03-26 12:10:41 -0600

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -30,7 +30,7 @@ def test_DVR_on_reboot():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(timeout)
     try:
-        sock.connect("30.255.240.82:65432")
+        sock.connect("30.255.240.82", port)
         sock.send(command0)          
     except Exception as ex:
         print(ex)
