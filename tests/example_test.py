c5d488e6e9d4b902d21e0c9c239d31432ac6b67f
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-03-26 14:00:45 -0600

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -56,7 +56,7 @@ def send_command_internal(command, box_ip, timeout=10.0):
 
 def test_DVR_on_reboot():
     print("TESTEST")
-    subprocess.call(['./test1.sh'])
+    p = subprocess.Popen('ls -lrt', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     count = 0
     while True:
      if stbt.is_screen_black(): break
