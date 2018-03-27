8b9dd2849e243cbe4460acb9f1517b706bbdf05c
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-03-27 11:51:35 -0600

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -65,12 +65,12 @@ child.sendline('Charter1')
 child.expect(r'.*', timeout=120)
 child.sendline('python dvr_status.py 30.255.240.82')
 child.sendline('exit')
-'''
     ssh = subprocess.Popen(["/usr/bin/ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@172.30.82.139", "ls -l"],
                            shell=False,
                            stdout=subprocess.PIPE,
                            stderr=subprocess.PIPE)
     result = ssh.stdout.readlines(
+    '''
     count = 0
     while True:
      if stbt.is_screen_black(): break
