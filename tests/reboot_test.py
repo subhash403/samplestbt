e8d1dd57628f46f6ea46382b77de95c02924dd46
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-03-29 15:44:21 -0600

    Update reboot_test.py

--- tests/reboot_test.py
+++ tests/reboot_test.py
@@ -13,6 +13,12 @@ import getpass
 import shutil
 import example_test
 
+def mydvr_launch():
+    stbt.press('KEY_EXIT')
+    stbt.press('KEY_MYDVR')
+    assert stbt.wait_until(lambda: stbt.match("images/dvr/my_dvr.png")), \
+    "MyDVR not launched"
+
 def test_DVR_on_reboot():
     count = 0
     while True:
