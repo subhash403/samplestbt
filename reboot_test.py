620d9332ff4b1afb14fb4aa9f8adb3c798677cfd
Parker Franks <c-parker.franks@charter.com>
2018-03-29 15:17:42 -0600

    snapshot

--- reboot_test.py
+++ reboot_test.py
@@ -3,9 +3,9 @@ import requests
 import time
 
 def main():
-    os.system("child = pexpect.spawn('./mototerm 30.255.240.82')")
-    os.system("child.expect('cmd2k mode is off', timeout=20)")
-    os.system("child.sendline('osdiag rebootnow')")
+    child = pexpect.spawn('./mototerm 30.255.240.82')
+    child.expect('cmd2k mode is off', timeout=20)
+    child.sendline('osdiag rebootnow')
     time.sleep(3)
     os.system("/Users/parkerfranks/Documents/GitHub/stb-tester-test-pack-charter/stbt_rig.py -v --node-id=stb-tester-00044b80f5f9 run tests/reboot_test.py::test_DVR_on_reboot > /Users/parkerfranks/Documents/results.txt")
     while True:
