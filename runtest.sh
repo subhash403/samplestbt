62088fe4be254591e86d05c6c75b29ed50927fa8
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-03-30 14:30:50 -0600

    Update runtest.sh

--- runtest.sh
+++ runtest.sh
@@ -3,6 +3,6 @@ for i in $(seq 1 2); do
  echo "*********************** Try: $i *******************************"
  sshpass -p "Charter1" ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@172.30.82.139 "python dvr_status.py 30.255.240.82"
  python stbt_rig.py -v --node-id=stb-tester-00044b80f5f9 run tests/reboot_test.py::test_DVR_on_reboot 
- sleep 5
+ sleep 20
  b8V_zD5Aaz1cGqPwYQAuDK9p9-6Am2JP
 done
