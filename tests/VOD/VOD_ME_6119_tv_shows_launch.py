6a391f930b20dc43139e3ef67f1a195e8e34d5a6
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-02 14:01:23 -0600

    Update VOD_ME_6119_tv_shows_launch.py

--- tests/VOD/VOD_ME_6119_tv_shows_launch.py
+++ tests/VOD/VOD_ME_6119_tv_shows_launch.py
@@ -1,4 +1,4 @@
-import "github.com/stb-tester/stb-tester-test-pack-charter/blob/master/tests/methods/VOD_Methods"
+import /stb-tester/stb-tester-test-pack-charter/blob/master/tests/methods/VOD_Methods
 
 def test_VOD_ME_6119_tv_shows_launch():
     init()
