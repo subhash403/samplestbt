4661f864bd10ba3af895c5eb90dbb4020e1d4a7d
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-02 11:52:27 -0600

    Update VOD_Methods.py

--- tests/methods/VOD_Methods.py
+++ tests/methods/VOD_Methods.py
@@ -5,3 +5,19 @@ def tv_shows_launch():
     stbt.press('KEY_ENTER')
     assert stbt.wait_until(lambda: stbt.match("images/vod/tv_shows_logo.png")), \
     "TV Shows not launched"
+    
+def movies_launch():
+    menu_launch()
+    stbt.press('KEY_CHANNELUP')
+    for _ in " "*4: stbt.press('KEY_DOWN')
+    stbt.press('KEY_ENTER')
+    assert stbt.wait_until(lambda: stbt.match("images/vod/movies_logo.png")), \
+    "Movies not launched"
+    
+def video_store_launch():
+    menu_launch()
+    stbt.press('KEY_CHANNELUP')
+    for _ in " "*5: stbt.press('KEY_DOWN')
+    stbt.press('KEY_ENTER')
+    assert stbt.wait_until(lambda: stbt.match("images/vod/video_store_logo.png")), \
+    "Video Store not launched"
