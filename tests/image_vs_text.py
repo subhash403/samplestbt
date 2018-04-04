c80f4958b32c571ec32408a3883a0e6afdc52c8d
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-04 13:29:28 -0600

    Update image_vs_text.py

--- tests/image_vs_text.py
+++ tests/image_vs_text.py
@@ -1,6 +1,12 @@
 from VOD_methods import tv_shows_launch
+from VOD_methods import video_store_launch
 from Menu_methods import menu_launch
 import stbt
 
 def test_run():
     tv_shows_launch()
+    
+        def title(self):
+        return stbt.ocr(
+            frame=self._frame,
+            region=stbt.Region(x=320, y=120, right=1220, bottom=190))
