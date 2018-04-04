fef962f22b360f8545c1e493af5fe951c4bc5860
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-04 13:48:17 -0600

    Update VOD_methods.py

--- tests/VOD_methods.py
+++ tests/VOD_methods.py
@@ -17,9 +17,9 @@ def movies_launch():
     assert stbt.wait_until(lambda: stbt.match("images/vod/movies_logo.png")), \
     "Movies not launched"
     
-def video_store_launch(self):
+def video_store_launch():
     menu_launch()
     stbt.press('KEY_CHANNELUP')
     for _ in " "*5: stbt.press('KEY_DOWN')
     stbt.press('KEY_ENTER')
-    assert stbt.ocr(frame=self._frame,region=stbt.Region(x=508, y=54, width=263, height=64)) == "Video Store"
+    assert stbt.ocr(region=stbt.Region(x=508, y=54, width=263, height=64)) == "Video Store"
