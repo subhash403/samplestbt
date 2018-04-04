4787e911708b9a42a4a49e61234f4062171dc3f5
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-04 13:22:21 -0600

    Create Menu_methods.py

---
+++ tests/Menu_methods.py
@@ -0,0 +1,7 @@
+import stbt
+
+def menu_launch():
+    for _ in " "*2: stbt.press('KEY_EXIT')
+    stbt.press('KEY_MENU')
+    assert stbt.wait_until(lambda: stbt.match("images/menu/menu_logo.png")), \
+    "Menu not launched"
