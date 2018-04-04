fe3ba37e865fa5e1888d888eb29d4165c338d334
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-04 13:04:02 -0600

    Update image_vs_text.py

--- tests/Setup/image_vs_text.py
+++ tests/Setup/image_vs_text.py
@@ -1,1 +1,7 @@
-
+def tv_shows_launch():
+    menu_launch()
+    stbt.press('KEY_CHANNELUP')
+    for _ in " "*3: stbt.press('KEY_DOWN')
+    stbt.press('KEY_ENTER')
+    assert stbt.wait_until(lambda: stbt.match("images/vod/tv_shows_logo.png")), \
+    "TV Shows not launched"
