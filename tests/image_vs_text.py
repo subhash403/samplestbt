d2c36d8c659fade1bf4a4f59cfaaa2165d4bc3a8
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-06 13:21:40 -0600

    Update image_vs_text.py

--- tests/image_vs_text.py
+++ tests/image_vs_text.py
@@ -1,7 +1,7 @@
 from VOD_methods import tv_shows_launch
 from VOD_methods import video_store_launch
 from Menu_methods import menu_launch
-from stbt import methods
+import methods
 import stbt
 import api_test
 
