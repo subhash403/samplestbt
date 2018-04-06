5b14e981c252617355d2dd705860a51334a13831
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-06 15:15:15 -0600

    Update api_test.py

--- tests/api_test.py
+++ tests/api_test.py
@@ -27,12 +27,22 @@ def HDAT_setting(value):
 	
 def moviescatalog():
 	token = get_auth_token()
-	url = "http://spectrum.engprod-charter.net/api/pub/videocatalogedge/services/v1/vod/ctec_a3h4/features/movie_vod/catalog?depth=20&folderContentLimit=100&channelLineupId=CC32-1&folderId=0&startIndex=0&maxResults=100"
+	url = "http://spectrum.engprod-charter.net/api/pub/videocatalogedge/services/v1/vod/ctec_a3h4/features/movie_vod/catalog?depth=20&folderContentLimit=100&channelLineupId=CA34-11&folderId=0&startIndex=0&maxResults=100"
 	headers={'X-CHARTER-SESSION':token, 'Content-Type':'application/json'}
 	req = requests.get(url, auth=('charternet', 'Chart3rn3t'),headers=headers)
 	assert req.status_code == 200
-	print req.text
+	#print req.text
 	assets = movies_parser(req.text)
+	return assets
+
+def videostorecatalog():
+	token = get_auth_token()
+	url = "http://spectrum.engprod-charter.net/api/pub/videocatalogedge/services/v1/vod/ctec_a3h4/features/video_store/catalog?depth=20&folderContentLimit=100&channelLineupId=CA34-11&folderId=0&startIndex=0&maxResults=100")
+	headers={'X-CHARTER-SESSION':token, 'Content-Type':'application/json'}
+	req = requests.get(url, auth=('charternet', 'Chart3rn3t'),headers=headers)
+	assert req.status_code == 200
+	#print req.text
+	assets = video_store_parser(req.text)
 	return assets
 
 def movies_parser(string):
@@ -48,7 +58,36 @@ def movies_parser(string):
 	asset = np.unique(assets)
 	return asset
 
-	
+def video_store_parser(string):
+	assert string
+	m = string.split("DeliveryId")
+	assets = []
+	for num in m:
+		str = num[3:-1]
+		id = str.split("'")
+		if 'ctec' in st and 'exit' not in st:
+			assets.append(st)
+	asset = np.unique(assets)
+	ids = []
+	for a in asset:
+		st = a.split("StartDate")
+		ids.append(st[0][0:-4])
+	return ids
+
+def Rent():
+	assets = videostorecatalog()
+	token = get_auth_token()
+	count =0
+	for asset in assets:
+		url = "http://spectrum.engprod-charter.net/api/pub/rentalsedge/rentals/purchase?type=arris&deliveryId="+asset
+		headers={'X-CHARTER-SESSION':token, 'Content-Type':'application/json'}
+		req = requests.post(url ,auth=('charternet', 'Chart3rn3t'),headers=headers)
+		#print req.status_code
+		assert req.status_code == 200
+		count +=1
+		if count >= int(n):
+			break
+			
 def Add_watchlist(n):
 	assets = moviescatalog()
 	token = get_auth_token()
@@ -68,4 +107,5 @@ def Add_watchlist(n):
 def test_parser():
 	HDAT_setting("Off")
 	Add_watchlist(7)
+	Rent(1)
 
