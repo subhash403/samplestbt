bf07a69c75b7193159a27d96b2273e65a3848bf5
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-06 13:29:33 -0600

    Update MyLibrary_ME_1218.py

--- tests/My Library/MyLibrary_ME_1218.py
+++ tests/My Library/MyLibrary_ME_1218.py
@@ -1,18 +1,17 @@
 import stbt
+from my_library import MyLibrary
 
 def test_MyLib_ME_1218
   # Step 1: valdiate session and liveTV, add asset to Expiring Soon if none
   init()
-  my_library_launch()
   ensure_expiring_soon()
   # Step 2: verify that asset in MyLibrary > Expiring Soon has a reachable ADP
-  find_lane("Expiring Soon")
+  MyLibrary.open().find_lane("Expiring Soon")
   press('KEY_ENTER')
   assert stbt.wait_until(lambda: stbt.match("images/vod/adp_blankspace.png")), \
   "ADP of asset in Expiring Soon not reached"
   # Step 3: verify that asset in MyLibrary > Watchlist has a reachable ADP
-  my_library_launch()
-  find_lane("Watchlist")
+  MyLibrary.open().find_lane("Watchlist")
   press('KEY_ENTER')
   assert stbt.wait_until(lambda: stbt.match("images/vod/adp_blankspace.png")), \
   "ADP of asset in Watchlist not reached"
