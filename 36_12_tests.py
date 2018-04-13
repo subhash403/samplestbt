5b8b512f99ab74b8733621e5a94a2eaab60c84fc
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-13 11:33:51 -0600

    Update 36_12_tests.py

--- 36_12_tests.py
+++ 36_12_tests.py
@@ -1,5 +1,6 @@
 import stbt
 
 channels = [1,2,3,4,5,6,7,8,9]
-for ch in channel:
-    KEY_PRESS(ch)
+for ch in channels:
+    for x in list(ch): stbt.press('KEY_'+'x')
+    assert stbt.wait_for_motion()
