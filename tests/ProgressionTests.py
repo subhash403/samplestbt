12fd63181c422066516acd3da145565c4efdd4f7
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-16 10:55:48 -0600

    Update ProgressionTests.py

--- tests/ProgressionTests.py
+++ tests/ProgressionTests.py
@@ -20,15 +20,15 @@ def test_modify_DVR_options_50plus_times():
         sleep(5)
         stbt.press('KEY_RECORD')
         sleep(2)
-        if stbt.wait_for_match('images/dvr/ch_bar_rec.png'):
+        if stbt.wait_until('images/dvr/ch_bar_rec.png'):
             stbt.press('KEY_EXIT')
             stbt.press('KEY_RECORD')
             sleep(2)
-        if stbt.wait_for_match('images/dvr/edit_ep_rec.png'):
+        if stbt.wait_until('images/dvr/edit_ep_rec.png'):
             stbt.press('KEY_RIGHT')
             assert stbt.wait_until(lambda: stbt.match("images/dvr/edit_ep_deleted.png")), \
         "Cannot edit recording setting"
-        elif stbt.wait_for_match('images/dvr/edit_ep_deleted.png'):
+        elif stbt.wait_until('images/dvr/edit_ep_deleted.png'):
             stbt.press('KEY_RIGHT')
             assert stbt.wait_until(lambda: stbt.match("images/dvr/edit_ep_rec.png")), \
         "Cannot edit recording setting"
