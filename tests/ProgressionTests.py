e12cd6c9b9b1e6791eff73ce8717c643757f748e
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-18 12:51:52 -0600

    Update ProgressionTests.py

--- tests/ProgressionTests.py
+++ tests/ProgressionTests.py
@@ -161,11 +161,11 @@ def test_DVR_playback_50plus_times_yes_TTS():
         "MyDVR not launched"
         while True:
             stbt.press('KEY_ENTER')
-        sleep(1)
-        if stbt.match('images/cta/watch.png') or stbt.match('images/cta/resume.png'): break
-        count += 1
-        assert count < 16, \
-        "Could not find recording to play in DVR page"
+            sleep(1)
+            if stbt.match('images/cta/watch.png') or stbt.match('images/cta/resume.png'): break
+            count += 1
+            assert count < 16, \
+            "Could not find recording to play in DVR page"
         stbt.press('KEY_ENTER')
         sleep(3)
         assert stbt.wait_for_motion(timeout_secs=20)
