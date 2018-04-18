7a7e571dc6a12dc71cbffeeb703e7b19ffe81fef
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-18 12:23:30 -0600

    Update ProgressionTests.py

--- tests/ProgressionTests.py
+++ tests/ProgressionTests.py
@@ -8,6 +8,9 @@ from dialogs import UnableToPlayDialog
 
 def mydvr_launch():
     stbt.press('KEY_EXIT')
+    stbt.press('KEY_EXIT')
+    stbt.press('KEY_EXIT')
+    sleep(1)
     stbt.press('KEY_MYDVR')
     assert stbt.wait_until(lambda: stbt.match("images/dvr/my_dvr.png")), \
     "MyDVR not launched"
