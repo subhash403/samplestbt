29a6f5f222e7b7414b888f559d914f25c7458738
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-19 13:27:37 -0600

    Update IP_Video_Tests.py

--- tests/IP_Video_Tests.py
+++ tests/IP_Video_Tests.py
@@ -4,35 +4,22 @@ from time import sleep
 def test_apis():
     stbt.press('KEY_RECORD')
     sleep(3)
-    stbt.press('KEY_DOWN')
-    sleep(1)
-    stbt.press('KEY_DOWN')
-    sleep(1)
-    stbt.press('KEY_DOWN')
-    sleep(1)
-    stbt.press('KEY_ENTER')
-    sleep(3)
-    stbt.press('KEY_RECORD')
-    sleep(3)
-    stbt.press('KEY_DOWN')
-    sleep(1)
-    stbt.press('KEY_ENTER')
     while True:
         stbt.press('KEY_UP')
         if stbt.wait_until(lambda: stbt.match("images/env/ip_video_top_option.png")):
             break
-           
+    stbt.press('KEY_DOWN')
+    sleep(1)
+    stbt.press('KEY_DOWN')
+    sleep(1)
     stbt.press('KEY_DOWN')
     sleep(1)
     stbt.press('KEY_ENTER')
-    while True:
-        stbt.press('KEY_LAST')
-        if stbt.wait_until(lambda: stbt.match("images/env/ip_video_top_option.png")):
-            break
-    #stbt.press('KEY_DOWN')
-    #sleep(1)
-    #stbt.press('KEY_DOWN')
-    #sleep(1)
-    #stbt.press('KEY_ENTER')
-    #sleep(10)
-    #stbt.press('KEY_REPLAY')
+    sleep(3)
+    assert stbt.wait_until(lambda: stbt.match("images/Select_True.png"))
+    assert stbt.wait_until(lambda: stbt.match("images/Menu_True.png"))
+    assert stbt.wait_until(lambda: stbt.match("images/Right_True.png"))
+    assert stbt.wait_until(lambda: stbt.match("images/Left_True.png"))
+    assert stbt.wait_until(lambda: stbt.match("images/Led_Down_True.png"))
+    assert stbt.wait_until(lambda: stbt.match("images/Led_Up_True.png"))
+
