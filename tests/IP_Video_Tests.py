68676a6f5161ceb9454c1defc06124ce69ac9d17
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-19 13:29:05 -0600

    Update IP_Video_Tests.py

--- tests/IP_Video_Tests.py
+++ tests/IP_Video_Tests.py
@@ -22,4 +22,13 @@ def test_apis():
     assert stbt.wait_until(lambda: stbt.match("images/Left_True.png"))
     assert stbt.wait_until(lambda: stbt.match("images/Led_Down_True.png"))
     assert stbt.wait_until(lambda: stbt.match("images/Led_Up_True.png"))
+    stbt.press('KEY_RECORD')
+    sleep(3)
+    while True:
+        stbt.press('KEY_UP')
+        if stbt.wait_until(lambda: stbt.match("images/env/ip_video_top_option.png")):
+            break
+    stbt.press('KEY_DOWN')
+    sleep(1)
+    stbt.press('KEY_DOWN')
 
