d89fbe2d9c6892b628d36265378ee5703abf23cf
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-19 13:19:35 -0600

    Update IP_Video_Tests.py

--- tests/IP_Video_Tests.py
+++ tests/IP_Video_Tests.py
@@ -4,6 +4,19 @@ from time import sleep
 def test_apis():
     stbt.press('KEY_RECORD')
     sleep(3)
+    stbt.press('KEY_DOWN')
+    sleep(1)
+    stbt.press('KEY_DOWN')
+    sleep(1)
+    stbt.press('KEY_DOWN')
+    sleep(1)
+    stbt.press('KEY_ENTER')
+    sleep(3)
+    stbt.press('KEY_RECORD')
+    sleep(3)
+    stbt.press('KEY_DOWN')
+    sleep(1)
+    stbt.press('KEY_ENTER')
     while True:
         stbt.press('KEY_UP')
         if stbt.wait_until(lambda: stbt.match("images/env/ip_video_top_option.png")):
