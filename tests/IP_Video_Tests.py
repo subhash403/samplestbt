4d3376e92a2154e1e2a8c7366e92d7762a7b20d9
prayfran2018 <37638871+prayfran2018@users.noreply.github.com>
2018-04-19 12:56:21 -0600

    Update IP_Video_Tests.py

--- tests/IP_Video_Tests.py
+++ tests/IP_Video_Tests.py
@@ -10,6 +10,10 @@ def test_apis():
         if stbt.wait_until(lambda: stbt.match("images/env/ip_video_top_option.png")):
             break
            
-    for _ in " "*3: stbt.press('KEY_DOWN')
+    stbt.press('KEY_DOWN')
+    sleep(3)
+    stbt.press('KEY_DOWN')
+    sleep(3)
+    stbt.press('KEY_DOWN')
     stbt.press('KEY_ENTER')
     stbt.press('KEY_REPLAY')
