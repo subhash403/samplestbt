9fc35191043b4a04d3dd51a24902df6c84e3be67
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-24 12:07:13 -0600

    Update ProgressionTests.py

--- tests/ProgressionTests.py
+++ tests/ProgressionTests.py
@@ -23,6 +23,10 @@ def test_modify_DVR_options_50plus_times():
         for _ in " "*2: stbt.press('KEY_EXIT')
         sleep(2)
         stbt.press('KEY_RECORD')
+        if not stbt.wait_until(lambda: stbt.match("images/dvr/edit_ep_rec.png")) and not stbt.wait_until_lambda: stbt.match("images/dvr/edit_ep_deleted.png")):
+            for _ in " "*2: stbt.press('KEY_EXIT')
+            sleep(2)
+            stbt.press('KEY_RECORD')
         if counter == 1 and stbt.wait_until(lambda: stbt.match("images/dvr/edit_ep_deleted.png")):
             stbt.press('KEY_RIGHT')
             stbt.press('KEY_UP')
@@ -57,6 +61,8 @@ def test_modify_DVR_from_miniG_50plus_times():
         for _ in " "*2: stbt.press('KEY_ENTER')
         sleep(1)
         stbt.press('KEY_RECORD')
+        if not stbt.wait_until(lambda: stbt.match("images/dvr/edit_ep_rec.png")) and not stbt.wait_until_lambda: stbt.match("images/dvr/edit_ep_deleted.png")):
+            stbt.press('KEY_RECORD')
         if counter == 1 and stbt.wait_until(lambda: stbt.match("images/dvr/edit_ep_deleted.png")):
             stbt.press('KEY_RIGHT')
             stbt.press('KEY_UP')
