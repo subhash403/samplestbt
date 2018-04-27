655b78858b149a91059819d78ae322b8dece48e4
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-27 13:29:31 -0600

    Update run_single.py

--- run_single.py
+++ run_single.py
@@ -2,11 +2,11 @@ import requests
 
 response = requests.post(
     "https://charter.stb-tester.com/api/v2/run_tests",
-    headers={"Authorization": "token BP7BxqZvuZPGeQRBuG6fxh7S_B1iWmS9"},
+    headers={"Authorization": "token W7rfXCJfEIlk1eeVywNmBfPvSGYYt_2l"},
     data=json.dumps({
-        "node_id": "stb-tester-abcdef123456",
-        "test_pack_revision": "c2c82cad",
-        "test_cases": ["tests/menu.py::test_that_menu_appears_when_menu_key_is_pressed"],
+        "node_id": "stb-tester-00044b80f5f9",
+        "test_pack_revision": "147b7d7",
+        "test_cases": ["tests/example_test.py::test_VOD_ME_6119_tv_shows_launch"],
         "remote_control": "roku",
         }))
 print response.json()
