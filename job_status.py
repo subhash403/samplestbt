18b6987e06a32872d284ced64035037d93ca62f9
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-27 14:43:48 -0600

    Update job_status.py

--- job_status.py
+++ job_status.py
@@ -2,6 +2,6 @@ import requests
 
 status = requests.get(
     "https://charter.stb-tester.com/api/v2/nodes/stb-tester-00044b80f5f9/job",
-    headers={"Authorization": "token BP7BxqZvuZPGeQRBuG6fxh7S_B1iWmS9"}
-    ).json()
+    headers={"Authorization": "token W7rfXCJfEIlk1eeVywNmBfPvSGYYt_2l"}
+    )
 print status
