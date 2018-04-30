4dfee8f9e7528f3dc7f822b6f99b582b7f0ba471
beauyoder <37630070+beauyoder@users.noreply.github.com>
2018-04-30 08:50:37 -0600

    Update get_results.py

--- get_results.py
+++ get_results.py
@@ -4,5 +4,5 @@ import json
 response = requests.get(
     "https://charter.stb-tester.com/api/v2/results",
     headers={"Authorization": "token W7rfXCJfEIlk1eeVywNmBfPvSGYYt_2l"},
-    params={"filter": "job:/stb-tester-00044b80f5f9"})
+    params={"filter": "job:/stb-tester-00044b80f5f9/74Vf/3747"})
 print response.json()
