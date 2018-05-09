4cb72a53c49b7508b0617520d2f3d48d130514b9
beauyoder <c-beau.yoder@charter.com>
2018-05-09 15:57:47 -0600

    Merge branch 'master' of https://github.com/stb-tester/stb-tester-test-pack-charter
    # Conflicts:
    #	tests/integration.py

--- tests/integration.py
+++ tests/integration.py
@@ -2,6 +2,7 @@ from charter_automation_repo.Astro.Video import Tester
 from charter_automation_repo.SpectrumGuide.ObjectRepo import AppsScreen
 
 def test_demo_framework():
-    Tester.press("KEY_MENU")
-    Tester.check_image(AppsScreen.Apps_Logo["image"])
-    Tester.check_text(AppsScreen.Apps_Logo["ocr"])
+    test = Tester()
+    test.press("KEY_MENU")
+    test.check_image(MainMenuScreen.Spectrum_Logo["logo"])
+    #test.check_text(AppsScreen.Apps_Logo["ocr"])
