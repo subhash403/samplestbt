de504a6cd688ed6b5ba2e7ccf253c9e47b9c9658
codecsiva <sivaramkumar.mani@charter.com>
2018-05-09 15:30:59 -0600

    Framework - Demo

--- tests/integration.py
+++
@@ -1,7 +0,0 @@
-from charter_automation_repo.Astro.Video import Tester
-from charter_automation_repo.SpectrumGuide.ObjectRepo import AppsScreen
-
-def test_demo_framework():
-    Tester.press("KEY_MENU")
-    Tester.check_image(AppsScreen.Apps_Logo["image"])
-    Tester.check_text(AppsScreen.Apps_Logo["ocr"])
