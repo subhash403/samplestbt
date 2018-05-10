18cedd5dee5873bf831be97260d5b14905f33f95
beauyoder <c-beau.yoder@charter.com>
2018-05-10 10:17:58 -0600

    Merge branch 'master' of https://github.com/stb-tester/stb-tester-test-pack-charter
    # Conflicts:
    #	tests/integration.py

--- tests/integration.py
+++ tests/integration.py
@@ -1,7 +1,20 @@
+import os
+import sys
+
+# Workaround for import path behaviour; can be removed once stb-tester v29 is
+# released.
+def _get_test_pack_root():
+    d = os.path.dirname(os.path.abspath(__file__))
+    while not os.path.exists(os.path.join(d, ".stbt.conf")):
+        d = os.path.dirname(d)
+    return d
+sys.path.insert(0, _get_test_pack_root())
+
 from charter_automation_repo.Astro.Video import Tester
 from charter_automation_repo.SpectrumGuide.ObjectRepo import AppsScreen
 
 def test_demo_framework():
-    Tester.press("KEY_MENU")
-    Tester.check_image(AppsScreen.Apps_Logo["image"])
-    Tester.check_text(AppsScreen.Apps_Logo["ocr"])
+    test = Tester()
+    test.press("KEY_MENU")
+    test.check_image(AppsScreen.Apps_Logo["image"])
+    test.check_text(AppsScreen.Apps_Logo["ocr"])
