aff470cc0710e31a44aaba4af86c91040e40ded1
beauyoder <c-beau.yoder@charter.com>
2018-05-14 13:34:09 -0600

    Merge branch 'develop' of https://github.com/stb-tester/stb-tester-test-pack-charter
    # Conflicts:
    #	.idea/workspace.xml

--- tests/SpectrumGuide/astro/Tester.py
+++ tests/SpectrumGuide/astro/Tester.py
@@ -1,6 +1,6 @@
 import stbt
 
-def press_untill_image_match(image_to_find, maximum_key_press=5):
+def press_until_image_match(image_to_find, maximum_key_press=5):
     return stbt.press_until_match(image_to_find, max_presses=maximum_key_press)
 
 def press(key, times=1):
@@ -12,8 +12,17 @@ def check_image(image_to_find,timeout_secs=10):
 def check_motion(timeout):
     return stbt.wait_for_motion(timeout)
 
+def get_text(region=None):
+    try:
+        txt_region = stbt.Region(region["x"], region["y"], region["width"], region["height"])
+        ocr_text = stbt.ocr(txt_region)
+        ocr_text.strip()
+        return ocr_text
+    except ValueError:
+        return None
+
 def check_text(text_to_check,region=None):
-    if not region is None:
+    if region is not None:
         txt_region = stbt.Region(region["x"], region["y"], region["width"], region["height"])
         ocr_txt = stbt.ocr(txt_region)
         ocr_txt.strip()
@@ -21,3 +30,8 @@ def check_text(text_to_check,region=None):
             return True
         else:
             return False
+
+def launch_guide():
+    press('KEY_GUIDE')
+    assert check_image(guide_options.png), \
+    "Guide not launched from Live TV within 10 seconds"
