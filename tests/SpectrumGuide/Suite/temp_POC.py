684f23a8eabd17420a6ba92947b6f562d825addc
beauyoder <c-beau.yoder@charter.com>
2018-05-15 10:57:13 -0600

    Merge branch 'develop' of https://github.com/stb-tester/stb-tester-test-pack-charter
    # Conflicts:
    #	.idea/workspace.xml

--- tests/SpectrumGuide/Suite/temp_POC.py
+++ tests/SpectrumGuide/Suite/temp_POC.py
@@ -6,10 +6,8 @@ from time import sleep
 # Workaround for import path behaviour; can be removed once stb-tester v29 is released.
 def _get_test_pack_root():
     d = os.path.dirname(os.path.abspath(__file__))
-    print "dirName : %s" % d
     while not os.path.exists(os.path.join(d, ".stbt.conf")):
         d = os.path.dirname(d)
-        print "dirName : %s" % d
     return d
 
 
