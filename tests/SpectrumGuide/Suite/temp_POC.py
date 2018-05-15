b1f33960bcc0360a62e096007c4cb29a10c6ed82
beauyoder <c-beau.yoder@charter.com>
2018-05-15 10:32:23 -0600

    Merge branch 'develop' of https://github.com/stb-tester/stb-tester-test-pack-charter
    # Conflicts:
    #	.idea/workspace.xml

--- tests/SpectrumGuide/Suite/temp_POC.py
+++ tests/SpectrumGuide/Suite/temp_POC.py
@@ -2,6 +2,7 @@ import os
 import sys
 from time import sleep
 
+
 # Workaround for import path behaviour; can be removed once stb-tester v29 is released.
 def _get_test_pack_root():
     d = os.path.dirname(os.path.abspath(__file__))
@@ -11,15 +12,16 @@ def _get_test_pack_root():
         print "dirName : %s" % d
     return d
 
-sys.path.insert(0, _get_test_pack_root())
 
+sys.path.insert(0, _get_test_pack_root())
 
 from tests.astro import Tester
 from tests.SpectrumGuide.ObjectRepo import MainMenuScreen
+
 
 def test_logging():
     Tester.LogResults.passed("Step 1 did a thing")
     Tester.remote_control_press('KEY_MENU')
     sleep(3)
     assert Tester.check_image(MainMenuScreen.Spectrum_Logo["image"]), \
-    "Main Menu logo not found"
+        "Main Menu logo not found"
