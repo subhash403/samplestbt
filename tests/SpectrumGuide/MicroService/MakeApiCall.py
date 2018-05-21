bac6052cd6f0871b7257252c166ddd58b55fc01e
Prasad Hegde <c-prasad.hegde@charter.com>
2018-05-22 00:26:11 +0530

    MakeApiCall Method

---
+++ tests/SpectrumGuide/MicroService/MakeApiCall.py
@@ -0,0 +1,11 @@
+class MakeApiCall():
+
+    def __init__(self):
+        """This is a constructor method for initializing MakeApiCall Object"""
+
+
+
+    def parental_pin(toggle):
+
+    def purchase_pin(toggle):
+        
