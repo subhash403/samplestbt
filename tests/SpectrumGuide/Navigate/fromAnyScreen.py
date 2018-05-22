879fd8c8005b05c137e99164d39191193bf8068a
codecsiva <sivaramkumar.mani@charter.com>
2018-05-22 14:51:49 -0600

    fixed reporting from go to live tv method

--- tests/SpectrumGuide/Navigate/fromAnyScreen.py
+++ tests/SpectrumGuide/Navigate/fromAnyScreen.py
@@ -4,7 +4,7 @@ import time
 def exit_to_live_tv_screen(step_name, user, number_of_exit_key=1,wait_after_key_press_secs=1):
     user.remote_control_press('KEY_EXIT', number_of_exit_key)
     if user.check_motion(motion_timeout=10) :
-        user.LogResults.passed("{} Exit from current screen to Live TV Screen using {} EXIT key".format(step_name, number_of_exit_key))
+        user.LogResults.passed("{}: Exit from current screen to Live TV Screen using {} EXIT key".format(step_name, number_of_exit_key))
         time.sleep(wait_after_key_press_secs)
         return True
     else:
