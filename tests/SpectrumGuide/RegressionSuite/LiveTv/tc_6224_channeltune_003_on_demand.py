04d8f6d1ef19bf5eed1c627a3e87118e3fe60b83
Administrator <rathnasubhash3@gmail.com>
2018-05-25 23:35:38 +0530

    updatedtc6224 import fromAnyScreen-8

--- tests/SpectrumGuide/RegressionSuite/LiveTv/tc_6224_channeltune_003_on_demand.py
+++ tests/SpectrumGuide/RegressionSuite/LiveTv/tc_6224_channeltune_003_on_demand.py
@@ -13,6 +13,7 @@ from tests.SpectrumGuide.ObjectRepo import TvShowsScreen
 from tests.SpectrumGuide.ObjectRepo import GuideScreen
 from tests.astro.Sampler import UserWrapper
 from tests.SpectrumGuide.Navigate import fromLiveTV
+from tests.SpectrumGuide.Navigate import fromAnyScreen
 from tests.SpectrumGuide.ObjectRepo import MainMenuScreen
 from tests.SpectrumGuide.ObjectRepo import fromminiguidescreen
 
