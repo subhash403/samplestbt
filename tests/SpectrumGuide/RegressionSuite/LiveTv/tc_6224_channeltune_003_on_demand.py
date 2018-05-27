1709ad1880afe50ec29085d1330763e929f839c4
Administrator <rathnasubhash3@gmail.com>
2018-05-27 11:49:58 +0530

    updatedtc6224 -12

--- tests/SpectrumGuide/RegressionSuite/LiveTv/tc_6224_channeltune_003_on_demand.py
+++ tests/SpectrumGuide/RegressionSuite/LiveTv/tc_6224_channeltune_003_on_demand.py
@@ -18,40 +18,7 @@ from tests.SpectrumGuide.ObjectRepo import MainMenuScreen
 from tests.SpectrumGuide.ObjectRepo import fromminiguidescreen
 
 user = UserWrapper()
-def ondemand_to_tv_shows(step_name, Tester):
-    if user.remote_control_press_until_image_match('KEY_ONDEMAND', TvShowsScreen.logo["image"], region=None,maximum_key_press=1):
-        user.LogResults.passed("Expected - TV Shows is Displayed, Actual- Displayed")
-        return True
-    else:
-        user.LogResults.failed("Expected - TV Shows is Displayed, Actual- Not Displayed")
-        return False
 
-
-def miniguidelaunch(step_name, Tester):
-    if user.remote_control_press_until_image_match('KEY_ENTER', fromminiguidescreen.logo["image"],
-                                                   region=None, maximum_key_press=2):
-        user.LogResults.passed("Expected - MiniGuide is Displayed, Actual- Displayed")
-        return True
-    else:
-        user.LogResults.failed("Expected - MiniGuide is Displayed, Actual- Not Displayed")
-        return False
-
-
-def checkchannelnumberinminiguide(number, channelnumber, key_press_times):
-    user.remote_control_press(number, key_press_times)
-    if not user.check_text(channelnumber):
-        user.clean_up(test_id, test_name)
-        return
-
-
-def Guidelaunch(step_name, Tester):
-    if user.remote_control_press_until_image_match('KEY_Guide', GuideScreen.guide_options["image"],
-                                                   region=None, maximum_key_press=1):
-        user.LogResults.passed("Expected - Guide is Displayed, Actual- Displayed")
-        return True
-    else:
-        user.LogResults.failed("Expected - Guide is Displayed, Actual- Not Displayed")
-        return False
 
 def test_tc_6224_ChannelTune_003_On_Demand():
     # Initialize test
@@ -61,6 +28,36 @@ def test_tc_6224_ChannelTune_003_On_Demand():
     user.start()
     user.LogResults.info("Test ID : {}, Test Name :{}".format(test_id, test_name))
     assertion_flag = True
+
+    def ondemand_to_tv_shows(step_name, Tester):
+        if user.remote_control_press_until_image_match('KEY_ONDEMAND', TvShowsScreen.logo["image"], region=None,maximum_key_press=1):
+            user.LogResults.passed("Expected - TV Shows is Displayed, Actual- Displayed")
+            return True
+        else:
+            user.LogResults.failed("Expected - TV Shows is Displayed, Actual- Not Displayed")
+            return False
+
+    def miniguidelaunch(step_name, Tester):
+        if user.remote_control_press_until_image_match('KEY_ENTER', fromminiguidescreen.logo["image"],region=None, maximum_key_press=2):
+            user.LogResults.passed("Expected - MiniGuide is Displayed, Actual- Displayed")
+            return True
+        else:
+            user.LogResults.failed("Expected - MiniGuide is Displayed, Actual- Not Displayed")
+            return False
+
+    def checkchannelnumberinminiguide(number, channelnumber, key_press_times):
+        user.remote_control_press(number, key_press_times)
+        if not user.check_text(channelnumber):
+            user.clean_up(test_id, test_name)
+            return
+
+    def Guidelaunch(step_name, Tester):
+        if user.remote_control_press_until_image_match('KEY_Guide', GuideScreen.guide_options["image"],region=None, maximum_key_press=1):
+            user.LogResults.passed("Expected - Guide is Displayed, Actual- Displayed")
+            return True
+        else:
+            user.LogResults.failed("Expected - Guide is Displayed, Actual- Not Displayed")
+            return False
 
 # User should be in Live Tv Initally while starting the Test case
     if not fromAnyScreen.exit_to_live_tv_screen("Step 1", user, number_of_exit_key=2, wait_after_key_press_secs=5):
