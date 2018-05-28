a4624ce78eb275c34a239b4ba4fcffccdd39beff
Administrator <rathnasubhash3@gmail.com>
2018-05-28 10:08:41 +0530

    updated 6224 test case-17

--- tests/SpectrumGuide/RegressionSuite/LiveTv/tc_6224_channeltune_003_on_demand.py
+++ tests/SpectrumGuide/RegressionSuite/LiveTv/tc_6224_channeltune_003_on_demand.py
@@ -46,9 +46,9 @@ def test_tc_6224_ChannelTune_003_On_Demand():
             return False
 
     def checkchannelnumberinminiguide(key,number_of_times,channelnumber):
-        Tester.remote_control_press(key,number_of_times)
-        if not Tester.check_text(channelnumber):
-            Tester.clean_up(test_id, test_name)
+        user.remote_control_press(key,number_of_times)
+        if not user.check_text(channelnumber):
+            user.clean_up(test_id, test_name)
             return
 
     def Guidelaunch(step_name, Tester):
