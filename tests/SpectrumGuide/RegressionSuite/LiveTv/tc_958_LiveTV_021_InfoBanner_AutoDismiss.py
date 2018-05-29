06a614be22edf0443a29af4fb0182415e0cf5dc4
Administrator <rathnasubhash3@gmail.com>
2018-05-29 11:13:59 +0530

    creating tc_958-5

--- tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
+++ tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
@@ -25,8 +25,8 @@ from tests.astro.Sampler import UserWrapper
 
 def menu_launch():
     for _ in " "*2: stbt.press('KEY_EXIT')
-    stbt.press('KEY_MENU')
-    assert stbt.wait_until(lambda: stbt.match("images/menu/menu_logo.png")), \
+    user.press('KEY_MENU')
+    assert user.wait_until(lambda: user.match("images/menu/menu_logo.png")), \
     "Menu not launched"
 
 def test_tc_958_LiveTV_021_InfoBanner_AutoDismiss():
