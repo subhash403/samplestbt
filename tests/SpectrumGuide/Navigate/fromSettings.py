913d813ac83dc4b1567cbe7feb7866e11eea04ff
Administrator <rathnasubhash3@gmail.com>
2018-05-29 17:03:30 +0530

    changed preferences to preference

--- tests/SpectrumGuide/Navigate/fromSettings.py
+++ tests/SpectrumGuide/Navigate/fromSettings.py
@@ -1,13 +1,13 @@
 from tests.SpectrumGuide.ObjectRepo import SettingsandsupportScreen
 
 def to_preference(step_name,Tester):
-    if Tester.remote_control_press_until_image_match('KEY_DOWN', SettingsandsupportScreen.preferences["selected"], menu_region,maximum_key_press=5):
+    if Tester.remote_control_press_until_image_match('KEY_DOWN', SettingsandsupportScreen.preference["selected"], menu_region,maximum_key_press=5):
         Tester.LogResults.passed("{} : Navigating to Preferences options in  Settings & Support".format(step_name))
     else:
         Tester.LogResults.failed("{} : Navigating to Preferences options in  Settings & Support".format(step_name))
         return False
     Tester.remote_control_press('KEY_ENTER')
-    if Tester.check_image(SettingsandsupportScreen.preferences["launched"]):
+    if Tester.check_image(SettingsandsupportScreen.preference["launched"]):
         Tester.LogResults.passed("Expected - Preferences is Displayed, Actual- Displayed")
         return True
     else:
