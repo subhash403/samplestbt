568b38a7a612395a7d50aa862b270a0bacf12a20
Administrator <rathnasubhash3@gmail.com>
2018-05-29 11:59:03 +0530

    creating tc_958-12

--- tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
+++ tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
@@ -1,29 +1,21 @@
 import os
 import sys
 from time import sleep
-
-
-# Workaround for import path behaviour; can be removed once stb-tester v29 is released.
+#Workaround for import path behaviour; can be removed once stb-tester v29 is released.
 def _get_test_pack_root():
     d = os.path.dirname(os.path.abspath(__file__))
     while not os.path.exists(os.path.join(d, ".stbt.conf")):
         d = os.path.dirname(d)
     return d
-
-
 sys.path.insert(0, _get_test_pack_root())
 from tests.cleanup.methods import Menu_Methods
-
-
-# from tests.SpectrumGuide.ObjectRepo import TvShowsScreen
-# from tests.SpectrumGuide.ObjectRepo import GuideScreen
+#from tests.SpectrumGuide.ObjectRepo import TvShowsScreen
+#from tests.SpectrumGuide.ObjectRepo import GuideScreen
 from tests.astro.Sampler import UserWrapper
-# from tests.SpectrumGuide.Navigate import fromLiveTV
-# from tests.SpectrumGuide.Navigate import fromAnyScreen
-# from tests.SpectrumGuide.ObjectRepo import MainMenuScreen
-# from tests.SpectrumGuide.ObjectRepo import fromminiguidescreen
-import stbt
-
+#from tests.SpectrumGuide.Navigate import fromLiveTV
+#from tests.SpectrumGuide.Navigate import fromAnyScreen
+#from tests.SpectrumGuide.ObjectRepo import MainMenuScreen
+#from tests.SpectrumGuide.ObjectRepo import fromminiguidescreen
 
 def test_tc_958_LiveTV_021_InfoBanner_AutoDismiss():
     # Initialize test
@@ -34,12 +26,15 @@ def test_tc_958_LiveTV_021_InfoBanner_AutoDismiss():
     user.LogResults.info("Test ID : {}, Test Name :{}".format(test_id, test_name))
     assertion_flag = True
 
-    def menu_launch():
-        for _ in " " * 2: stbt.press('KEY_EXIT')
-        stbt.press('KEY_MENU')
-        assert stbt.wait_until(lambda: stbt.match("images/menu/menu_logo.png")), \
-            "Menu not launched"
-
-    if not menu_launch():
+    def to_menu(step_name,Tester):
+        Tester.remote_control_press('KEY_MENU')
+        if Tester.check_image(MainMenuScreen.Spectrum_Logo["image"]):
+            Tester.LogResults.passed("Expected - MENU is Displayed, Actual- Displayed")
+            return True
+        else:
+            Tester.LogResults.failed("Expected - MENU is Displayed, Actual- Not Displayed")
+            return False
+    if not to_menu("step 1", user):
         user.clean_up(test_id, test_name)
         return
+
