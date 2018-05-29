8a4e59937954d8e7958e14b0e57c61de5a6ee08f
Administrator <rathnasubhash3@gmail.com>
2018-05-29 14:55:56 +0530

    settings 13

---
+++ tests/SpectrumGuide/ObjectRepo/SettingsandsupportScreen.py
@@ -0,0 +1,7 @@
+import os
+
+IMG_ABS_PATH = str(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+logo = {
+    "image": IMG_ABS_PATH + "/ImageRepo/ImageRepo/Settings/settingslaunched.png"
+}
