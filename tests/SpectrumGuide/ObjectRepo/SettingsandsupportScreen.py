95ab3cb62e1abfd89a2cf89143a5562e19bfa4a8
Administrator <rathnasubhash3@gmail.com>
2018-05-29 16:38:14 +0530

    changed Settingsandsupport file

--- tests/SpectrumGuide/ObjectRepo/SettingsandsupportScreen.py
+++ tests/SpectrumGuide/ObjectRepo/SettingsandsupportScreen.py
@@ -3,5 +3,5 @@ import os
 IMG_ABS_PATH = str(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 logo = {
-    "image": IMG_ABS_PATH + "/ImageRepo/ImageRepo/Settings/settingslaunched.png"
+    "image": IMG_ABS_PATH + "/ImageRepo/Settings/settingslaunched.png"
 }
