394364a3728334ea39638f07e5a5de1163b14228
Administrator <rathnasubhash3@gmail.com>
2018-05-29 16:06:41 +0530

    changed mainmenuscreen 12

--- tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
+++ tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
@@ -13,7 +13,7 @@ from tests.cleanup.methods import Menu_Methods
 #from tests.SpectrumGuide.ObjectRepo import GuideScreen
 from tests.astro.Sampler import UserWrapper
 #from tests.SpectrumGuide.Navigate import fromLiveTV
-#from tests.SpectrumGuide.Navigate import fromAnyScreen
+from tests.SpectrumGuide.Navigate import fromAnyScreen
 from tests.SpectrumGuide.ObjectRepo import MainMenuScreen
 from tests.SpectrumGuide.Navigate import fromMainMenu
 #from tests.SpectrumGuide.ObjectRepo import fromminiguidescreen
