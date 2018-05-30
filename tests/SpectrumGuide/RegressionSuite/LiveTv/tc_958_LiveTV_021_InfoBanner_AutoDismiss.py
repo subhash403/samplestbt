802e7c87cd086d5855707831f3cd1ee02a906081
Administrator <rathnasubhash3@gmail.com>
2018-05-30 15:09:26 +0530

    added testcase 4

--- tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
+++ tests/SpectrumGuide/RegressionSuite/LiveTv/tc_958_LiveTV_021_InfoBanner_AutoDismiss.py
@@ -51,8 +51,7 @@ def test_tc_958_LiveTV_021_InfoBanner_AutoDismiss():
                 return False
 
     def info_banner(step_name, Tester):
-        Tester.remote_control_press_until_image_match('KEY_DOWN', preferencesscreen.infobanner["image"], 7,
-                                                      menu_region):
+        Tester.remote_control_press_until_image_match('KEY_DOWN', preferencesscreen.infobanner["image"], 7,menu_region):
         Tester.remote_control_press('KEY_ENTER', 0)
         Tester.get_text(self, preferencesscreeninfo_banner_time.["region"]):
 
