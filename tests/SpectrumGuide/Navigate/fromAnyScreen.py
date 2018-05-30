cf5848df6b12fc2c4f204bf64c88b4cfa27754b7
beauyoder <c-beau.yoder@charter.com>
2018-05-30 15:34:14 -0600

    Merge branch 'master' of https://github.com/stb-tester/stb-tester-test-pack-charter

--- tests/SpectrumGuide/Navigate/fromAnyScreen.py
+++ tests/SpectrumGuide/Navigate/fromAnyScreen.py
@@ -22,3 +22,22 @@ def exit_to_live_tv_screen(step_name, user, number_of_exit_key=1, wait_after_key
         user.LogResults.failed("{} To Exit from current screen to Live TV Screen".format(step_name, number_of_exit_key))
         return False
 
+
+def turn_box_on(step_name, user, wait_after_power_press_secs=10):
+    """
+    :author: Beau Yoder
+    :description: presses Power if black screen is found
+    :param step_name:
+    :param user:
+    :param wait_after_power_press_secs:
+    :return:
+    """
+    if user.check_motion(motion_time_out_secs=10):
+        user.LogResults.passed("{}: Box detected as powered ON".format(step_name))
+        return True
+    if user.check_image(LiveTV.black_screen["image"]):
+        user.remote_control_press('KEY_POWER')
+        time.sleep(wait_after_power_press_secs)
+    if user.check_image(LiveTV.black_screen["image"]):
+        user.LogResults.failed("{}: Box unable to turn ON".format(step_name))
+        return False
