6a0753353757fa072b319b5c3249eb12276c4dfe
beauyoder <c-beau.yoder@charter.com>
2018-05-30 14:03:33 -0600

    Merge branch 'master' of https://github.com/stb-tester/stb-tester-test-pack-charter

--- tests/cleanup/VOD/VOD_ME_4434_check_VODpage_not_in_Recently_Watched.py
+++
@@ -1,9 +0,0 @@
-def test_VOD_ME_4434_check_VODpage_not_in_Recently_Watched():
-    tv_shows_launch()
-    time.sleep(65)
-    stbt.press('KEY_EXIT')
-    my_library_launch()
-    while True:
-     stbt.press('KEY_DOWN')
-     if stbt.wait_for_match('images/lane/recently_watched.png'): break      
-    stbt.press('KEY_ENTER')
--- tests/cleanup/VOD/VOD_ME_6119_tv_shows_launch.py
+++
@@ -1,6 +0,0 @@
-import VOD_Methods
-import dut_init
-
-def test_VOD_ME_6119_tv_shows_launch():
-    init()
-    tv_shows_launch()
