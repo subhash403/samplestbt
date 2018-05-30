e5ed6c19cd3588108c852d997c7a9bb8f9a1ce9e
Administrator <rathnasubhash3@gmail.com>
2018-05-30 22:54:05 +0530

    added preferences screen region

--- tests/SpectrumGuide/ObjectRepo/preferencesscreen.py
+++ tests/SpectrumGuide/ObjectRepo/preferencesscreen.py
@@ -2,7 +2,7 @@ import os
 
 IMG_ABS_PATH = str(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 area = {
-"region": {"x": 504, "y": 404, "width": 65, "height": 39}
+"region": {"x": 501, "y": 400, "width": 120, "height": 52}
 }
 
 guide_Settings = {
@@ -23,7 +23,6 @@ displayduration_launched = {
 
 infobanner = {
     "image": IMG_ABS_PATH +"/ImageRepo/preferences/Infobanner.png"
-
 }
 info_banner_time = {
     "region": {"x": 420, "y": 405, "width": 80, "height": 37}
