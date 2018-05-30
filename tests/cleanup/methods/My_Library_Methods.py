463b0620b2dc75e65b2ed8a15bafc319a09a29a3
beauyoder <c-beau.yoder@charter.com>
2018-05-30 14:00:33 -0600

    Merge branch 'master' of https://github.com/stb-tester/stb-tester-test-pack-charter

--- tests/cleanup/methods/My_Library_Methods.py
+++
@@ -1,7 +0,0 @@
-def my_library_launch():
-    menu_launch()
-    stbt.press('KEY_CHANNELUP')
-    for _ in " "*2: stbt.press('KEY_DOWN')
-    stbt.press('KEY_ENTER')
-    assert stbt.wait_until(lambda: stbt.match("images/vod/my_library_header.png")), \
-    "My Library not launched"
--- tests/cleanup/methods/Search_Methods.py
+++
@@ -1,6 +0,0 @@
-def search_launch():
-    menu_launch()
-    stbt.press('KEY_CHANNELUP')
-    stbt.press('KEY_ENTER')
-    assert stbt.wait_until(lambda: stbt.match("images/menu/search_logo.png")), \
-    "Search not launched"
