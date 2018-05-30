3fc4f4dfb0ee900603f07e1e01ae74db523a6622
codecsiva <sivaramkumar.mani@charter.com>
2018-05-30 14:27:14 -0600

    check motion parm rename

--- tests/SpectrumGuide/Navigate/fromAnyScreen.py
+++ tests/SpectrumGuide/Navigate/fromAnyScreen.py
@@ -13,7 +13,7 @@ def exit_to_live_tv_screen(step_name, user, number_of_exit_key=1, wait_after_key
     :return:
     """
     user.remote_control_press('KEY_EXIT', number_of_exit_key)
-    if user.check_motion(motion_timeout=10):
+    if user.check_motion(motion_time_out_secs=10):
         user.LogResults.passed("{}: Exit from current screen to Live TV Screen using {} EXIT key".format
                                (step_name, number_of_exit_key))
         time.sleep(wait_after_key_press_secs)
