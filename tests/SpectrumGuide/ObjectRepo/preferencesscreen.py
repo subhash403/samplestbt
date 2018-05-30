40b9c0a798950ce392dcda347b284a7617951897
Administrator <rathnasubhash3@gmail.com>
2018-05-30 16:37:31 +0530

    added changes of tc958

--- tests/SpectrumGuide/ObjectRepo/preferencesscreen.py
+++ tests/SpectrumGuide/ObjectRepo/preferencesscreen.py
@@ -20,7 +20,9 @@ displayduration_launched = {
 
 infobanner = {
     "image": IMG_ABS_PATH +"/ImageRepo/preferences/infobanner.png"
-    "region": {"x": 504, "y": 404, "width": 65, "height": 39}
+    "region" = {
+        "x": 504, "y": 404, "width": 65, "height": 39
+}
 }
 info_banner_time = {
     "region": {"x": 420, "y": 405, "width": 80, "height": 37}
