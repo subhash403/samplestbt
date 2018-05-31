c7234ad5c6676afed64ab854e7e4b5ce68d72bf8
Administrator <rathnasubhash3@gmail.com>
2018-05-31 18:57:39 +0530

    changed path of the images in Miniguide

--- tests/SpectrumGuide/ObjectRepo/MiniGuideScreen.py
+++ tests/SpectrumGuide/ObjectRepo/MiniGuideScreen.py
@@ -1,10 +1,10 @@
 import os
-IMG_ABS_PATH = str(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+IMG_ABS_PATH = "{}/ImageRepo/{}/".format(str(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),configuration.img_repo_version)
 
 logo = {
-    "image": IMG_ABS_PATH + "/ImageRepo/MiniGuide/miniguide.png"
+    "image": IMG_ABS_PATH + "MiniGuide/miniguide.png"
 }
 
 miniguide_ondemand = {
-      "image": IMG_ABS_PATH + "/ImageRepo/MiniGuide/miniguide_ondemand.png"
+      "image": IMG_ABS_PATH + "MiniGuide/miniguide_ondemand.png"
 }
